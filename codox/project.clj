87bb0bcefb5b3b9db58a4231b2540a498ac9579d
Peter Taoussanis <peter@taoensso.com>
2023-07-03 15:59:46 +0200

    Release 1.10.9 (fork)

--- codox/project.clj
+++ codox/project.clj
@@ -1,4 +1,4 @@
-(defproject codox "0.10.8"
+(defproject com.taoensso.forks/codox "0.10.9"
   :description "Generate documentation from Clojure source files"
   :url "https://github.com/weavejester/codox"
   :scm {:dir ".."}
